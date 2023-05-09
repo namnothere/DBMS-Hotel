6b4d2f7b6dde5f07002b7310363a76d50039ec2c
Nem <canhnamtran216@gmail.com>
2023-05-09 12:48:23 +0700

    grant perms in login

--- Hotel/DAO/MyDB.cs
+++ Hotel/DAO/MyDB.cs
@@ -22,6 +22,25 @@ namespace Hotel
 
         public MyDB(string username, string password)
         {
+            try
+            {
+                con = new SqlConnection();
+                String connectionStr = @"Data Source=NEM\DBMS_FINAL;Initial Catalog=Hotel;Trusted_Connection=True;User Id=" + username + ";Password=" + password;
+                con = new SqlConnection(connectionStr);
+                SqlCommand comm = con.CreateCommand();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+
+
+        }
+
+        public MyDB() { }
+
+        public MyDB(string username, string password)
+        {
             String connectionStr = @"Data Source=NEM\DBMS_FINAL;Initial Catalog=Hotel;USER ID=" + username + ";Password="+password;
             con = new SqlConnection(connectionStr);
             SqlCommand comm = con.CreateCommand();
